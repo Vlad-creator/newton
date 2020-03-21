1f488520b8b139b1d6f791a939677ff70068ccb3
Vlad-creator <56085984+Vlad-creator@users.noreply.github.com>
2020-03-21 20:53:35 +0300

    Update newton.c

--- newton.c
+++ newton.c
@@ -17,31 +17,34 @@ int main()
 {
 	fder_t f = &fder_ttt;
 	double res = newton(f , 1);
-	printf ("%.4f" , res);
+	printf ("%f\n" , res);
 	return 0;
 }
 
 double newton(fder_t f , double x)
 {
-	double x_new = x;
-	double x_old = 2;
-	while (1 != 0)
+	double x_new = 0;
+	double x_old = x;
+	int i = 0;
+	while (fabs(x_old - x_new) >  __DBL_EPSILON__)
 	{
-		if (fabs(x_old - x_new) <  0.000000001)
-			return x_new;
+		i++;
 		struct func_deriv func_need = f(x_old);
+		double per = x_old;
 		x_old = x_new;
-		x_new = x_old - func_need.func / func_need.der;
+		x_new = per - func_need.func / func_need.der;
 	};
-	return 0;
+	printf("newton - %d\n" , i);
+	return x_new;
 };
 
 struct func_deriv fder_ttt (double x)
 {
-	double a =  x * x + exp(x) - 0.827185;
-	double b =  2 * x + exp(x);
+	double a =  x * x * sin(x) - 5 * x + 7;
+	double b =  2 * x * sin(x) + x * x * cos(x) - 5;
 	struct func_deriv res;
 	res.func = a;
 	res.der = b;
 	return res;
 };
+
